cad71b7d63370f90bfa9744e0b02f714c337e89d
thorntonbrenden@gmail.com <thorntonbrenden@gmail.com>
2018-05-14 12:38:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
 
 function deleteFromObjectByKey(recipes, key){
   delete newRecipes.key;
-  return recipes;
+  return newRecipes;
 }
