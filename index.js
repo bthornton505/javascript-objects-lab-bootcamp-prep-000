581b818273da35cd00be979977b63a2fe6213446
thorntonbrenden@gmail.com <thorntonbrenden@gmail.com>
2018-05-14 12:29:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const recipes = {key: 'value'};
 
 function updateObjectWithKeyAndValue(recipes, key, value){
-  return Object.assign({}, recipes, [key] = value;
+  return Object.assign({}, recipes, {[key] = value;
 }
